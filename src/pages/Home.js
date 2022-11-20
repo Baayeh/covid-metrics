e3578d2866f773513ae19b9f4521f5b1aba9c017
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-20 08:28:11 +0000

    Provide store to components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getWorldData } from '../redux/world-data/worldSlice';
 
-const Home = () => <div>Home</div>;
+const Home = () => {
+  const worldData = useSelector((state) => state.worldData.data);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getWorldData());
+  }, []);
+
+  console.log(worldData);
+
+  return <div>some data</div>;
+};
 
 export default Home;
