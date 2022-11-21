d2c0e356ffd1ba2e9d07339d97f23e274bd25424
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-21 08:11:51 +0000

    refactor code

---
+++ src/components/dashboard/World.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const World = ({ currentWorldData }) => (
+  <section className="flex flex-wrap justify-center gap-4 p-2">
+    <div className="total-cases flex flex-col justify-center h-[6rem] text-center rounded p-5 w-[10rem]">
+      <span className="font-bold text-xl text-green-300">
+        {currentWorldData.TotalCases}
+      </span>
+      <span>Total Cases</span>
+    </div>
+    <div className="total-cases flex flex-col justify-center h-[6rem] text-center rounded p-5 w-[10rem]">
+      <span className="font-bold text-xl text-yellow-100">
+        {currentWorldData.ActiveCases}
+      </span>
+      <span>Active Cases</span>
+    </div>
+    <div className="total-cases flex flex-col justify-center h-[6rem] rounded p-5 text-center w-[10rem]">
+      <span className="font-bold text-xl text-red-800">
+        {currentWorldData.TotalDeaths}
+      </span>
+      <span>Total Deaths</span>
+    </div>
+    <div className="total-cases flex flex-col justify-center h-[6rem] rounded p-5 text-center w-[10rem]">
+      <span className="font-bold text-xl text-green-300">
+        {currentWorldData.NewCases}
+      </span>
+      <span>New Cases</span>
+    </div>
+    <div className="total-cases flex flex-col justify-center h-[6rem] rounded p-5 text-center w-[10rem]">
+      <span className="font-bold text-xl text-yellow-500">
+        {currentWorldData.Serious_Critical}
+      </span>
+      <span>Critical</span>
+    </div>
+    <div className="total-cases flex flex-col justify-center h-[6rem] rounded p-5 text-center w-[10rem]">
+      <span className="font-bold text-xl text-red-800">
+        {currentWorldData.NewDeaths}
+      </span>
+      <span>New Deaths</span>
+    </div>
+  </section>
+);
+
+World.propTypes = {
+  currentWorldData: PropTypes.shape({
+    TotalCases: PropTypes.number,
+    ActiveCases: PropTypes.number,
+    TotalDeaths: PropTypes.number,
+    NewCases: PropTypes.number,
+    Serious_Critical: PropTypes.number,
+    NewDeaths: PropTypes.number,
+  }).isRequired,
+};
+
+export default World;
