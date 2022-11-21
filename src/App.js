0b6984635e50ca68bbccd2652a958557a808435d
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-21 21:05:20 +0000

    merge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
+      {
+        location.pathname !== '/' ? (<NavBar />) : null
+      }
       <main>
         <Outlet />
       </main>
