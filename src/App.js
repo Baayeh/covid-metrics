71337c67a6a63d631abfc11f45cee10f7aaf5438
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-23 10:48:34 +0000

    Fix conflicts

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { NavBar } from './components';
 function App() {
   const location = useLocation();
   return (
-    <div className="App">
-      {
-        location.pathname !== '/' ? (<NavBar />) : null
-      }
-      <main>
-        <Outlet />
-      </main>
+    <div className="App md:flex md:relative">
+      {location.pathname !== '/' ? <NavBar /> : null}
+      {location.pathname === '/' ? (
+        <main className="w-full">
+          <Outlet />
+        </main>
+      ) : (
+        <main className="md:w-[70%] xl:w-[80%] md:ml-[30%] xl:ml-[20%]">
+          <Outlet />
+        </main>
+      )}
     </div>
   );
 }
