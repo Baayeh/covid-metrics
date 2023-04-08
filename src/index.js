e3f19903e09b769175012d56fa28f3163dcd7f5e
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-04-08 23:19:26 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
